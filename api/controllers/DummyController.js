9475f6d89b7c876ac2ea566a2896f8f230c5c9b1
Adam <guernica0131@gmail.com>
2014-05-23 13:21:48 +0500

    Intial Commit. Project intialized

---
+++ api/controllers/DummyController.js
@@ -0,0 +1,11 @@
+/**
+ * DummyController
+ *
+ * @description :: Server-side logic for managing Dummies
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
